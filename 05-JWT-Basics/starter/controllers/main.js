149ab78d1da5fa44b842b64d563286ddd105a380
Marcus <marcus.gamboa@gmail.com>
2021-11-22 14:38:39 -0700

    Add jwt code

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -20,10 +20,19 @@ const login = async (req, res) => {
 };
 
 const dashboard = async (req, res) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    throw new CustomAPIError(`Not token provided`, 400);
+  }
+
+  const token = authHeader.split(" ")[1];
+  console.log(token);
+
   const luckyNumber = Math.random() * 100;
   res.status(200).json({
     msg: `Hello John Doe`,
-    secret: `Here is your authorized data, you r lucky number is ${luckyNumber}`,
+    secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
   });
 };
 
